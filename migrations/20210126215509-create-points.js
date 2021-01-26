a262e170acffce4175a640c606cebf6c15edf8ce
Matt Phillips <spectremp24@gmail.com>
2021-01-26 17:16:26 -0500

    sequelize

---
+++ migrations/20210126215509-create-points.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('points', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      points: {
+        type: Sequelize.INTEGER
+      },
+      userID: {
+        type: Sequelize.INTEGER,
+        references: {
+          models: 'users',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('points');
+  }
+};
